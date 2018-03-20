fec06b2318520242a957b5e627cab4de5765ab0e
CPU60029-local <dvt1996@yahoo.com.vn>
2018-03-20 11:02:24 +0700

    Update

---
+++ src/main/java/letschat/server/trung/Client/NettyClient.java
@@ -0,0 +1,47 @@
+package Client;
+
+import Server.RequestData;
+import Server.ResponseData;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.MessageToByteEncoder;
+import io.netty.handler.codec.ReplayingDecoder;
+
+import java.nio.charset.Charset;
+import java.util.List;
+
+public class NettyClient {
+    public static void main(String[] args) throws Exception {
+
+        String host = "localhost";
+        int port = 8080;
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(workerGroup);
+            b.channel(NioSocketChannel.class);
+            b.option(ChannelOption.SO_KEEPALIVE, true);
+            b.handler(new ChannelInitializer<SocketChannel>() {
+
+                @Override
+                public void initChannel(SocketChannel ch)
+                        throws Exception {
+                    ch.pipeline().addLast(new RequestDataEncoder(),
+                            new ResponseDataDecoder(), new ClientHandler());
+                }
+            });
+
+            ChannelFuture f = b.connect(host, port).sync();
+
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
+
