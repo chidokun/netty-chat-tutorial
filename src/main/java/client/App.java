bcf9f4ab436dfb54fa8e373dc90273e7dbf063c7
Tuan. Nguyen Le Hoang <tuannlh@vng.com.vn>
2018-03-20 10:20:26 +0700

    Init commit

---
+++ src/main/java/client/App.java
@@ -0,0 +1,7 @@
+package client;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/letschat/App.java
@@ -0,0 +1,10 @@
+package letschat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+}
